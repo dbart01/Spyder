7110e946b37a5eb7d34035716c7d69710cbf6baa
Dima Bart <dima.bart01@gmail.com>
2016-02-25 17:48:14 -0500

    Initial Commit

---
+++ Spyder/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Spyder
+//
+//  Created by Dima Bart on 2016-02-25.
+//  Copyright © 2016 Dima Bart. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
