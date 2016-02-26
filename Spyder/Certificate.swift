946b5567e7271e8e3bdc70b556ea385fb5734802
Dima Bart <dima.bart01@gmail.com>
2016-02-26 14:03:26 -0500

    Add Certificate.

--- Spyder/Certificate.swift
+++ Spyder/Certificate.swift
@@ -7,3 +7,47 @@
 //
 
 import Foundation
+
+class Certificate {
+    
+    private(set) var certificate: SecCertificate!
+    private(set) var identity: SecIdentity!
+    
+    // ----------------------------------
+    //  MARK: - Init -
+    //
+    init?(path: String, passphrase: String) {
+        
+        if let path = path as NSString?,
+            let certificateData = NSData(contentsOfFile: path.stringByExpandingTildeInPath) {
+            
+            let options = [
+                kSecImportExportPassphrase as String : passphrase,
+            ]
+            
+            var items: CFArray?
+            let result = SecPKCS12Import(certificateData, options, &items)
+            guard result == errSecSuccess else {
+                return nil
+            }
+            
+            guard let certificates = items as Array<AnyObject>? where certificates.count > 0 else {
+                return nil
+            }
+            
+            guard let identityDictionary = certificates.first as? Dictionary<String, SecIdentity> else {
+                return nil
+            }
+            
+            self.identity = identityDictionary[kSecImportItemIdentity as String]!
+            
+            var cert: SecCertificate?
+            SecIdentityCopyCertificate(identity, &cert)
+            
+            self.certificate = cert!
+            
+        } else {
+            return nil
+        }
+    }
+}
