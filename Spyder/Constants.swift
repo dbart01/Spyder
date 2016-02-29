f897d9e2883ebca0602567f1f8951a47c6342157
Dima Bart <dima.bart01@gmail.com>
2016-02-29 11:09:04 -0500

    Add help description for expiry.

--- Spyder/Constants.swift
+++ Spyder/Constants.swift
@@ -60,6 +60,7 @@ let HelpContents = [
     " [-] -L, --payload     A JSON string representing the payload, or path to file with JSON payload",
     " [x] -T, --topic       Topic for which to send push. Often a bundle id in the form of 'com.company.app.voip'",
     " [ ]     --priority    Notification priority is either 10 or 5. Defaults to 10 if not provided",
+    " [ ] -x, --expiry      Unix timestamp indicating when the notification expires",
     "",
     
 ].joinWithSeparator("\n")
